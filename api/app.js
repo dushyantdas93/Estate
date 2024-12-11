7f8ab672f6f731ff995f08e52d9d568216c0a353
LokeshwarPrasad3 <lokeshwar.prasad.cse@gmail.com>
2024-12-11 22:45:16 +0530

    first commit

---
+++ api/app.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
+dotenv.config({
+  path:"./.env",
+})   
+  
+const app = express();
+const PORT = process.env.PORT || 4000
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get("/user",(req,res)=>{
+  res.json({name:"tiger",age:22})
+})
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(4000, () => {
+  console.log("Server is running!");
+});
