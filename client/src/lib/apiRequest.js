210ef30dd4a701f14c8da5c8e8178d95c1c4aa06
LokeshwarPrasad3 <lokeshwar.prasad.cse@gmail.com>
2024-12-11 23:09:49 +0530

    add backend url

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://my-estate-server.onrender.com/api",
   withCredentials: true,
 });
 
